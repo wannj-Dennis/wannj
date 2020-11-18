8db1906eb86fda3609aeafc2fef67d6acd7cd483
Niju <wanjofu26@live.com>
2020-11-18 17:14:06 +0000

    pre launch desktop first update

--- main.js
+++ main.js
@@ -2,16 +2,29 @@
 // set timeout
 document.getElementById('head-one').innerHTML = 'Wanj Dennis';
 
+// count 
+let marker = 0; 
+ const count = (projects) => {
+     setInterval (() => {
+         if (marker != projects){
+             marker += 1;
+             document.getElementById('projects').innerHTML = "<i class='fas fa-medal'></i> Projects " + '<span class="righter">' + marker + '</span>';
+         }
+     }, 500);
+ }
 
-    
+ count(5);
+
 setTimeout(
     function () {        
-        let val = document.getElementById('full-page');
-        val.style.height = '0';
-        let val_text = document.getElementById('head-one');
-        val_text.opacity = 0 ;
+
         let vals = document.getElementById('home-page');
         vals.style.opacity = '1';
+
+
+        let val = document.getElementById('full-page');
+        document.getElementById('head-one').style.display = 'none';
+        val.style.height = '0';        
         }, 2000
     );
 
@@ -40,7 +53,34 @@ document.getElementById('two').onclick = () => {
     thrice.getElementsByTagName('div')[0].setAttribute('id', 'slider2');
 }
 
-document.getElementById('three').onclick = () => {
+/*
+const toggleDisplay = (id) => {
+    if (document.getElementById('btn1').clicked == id){
+        document.getElementById('three').style.display = 'none';
+        document.getElementById('three-half').style.display = 'flex';
+    }
+    else if (document.getElementById('btn2').clicked == id)  {
+        document.getElementById('three-half').style.display = 'none';
+        document.getElementById('three').style.display = 'flex';      
+    }
+}
+
+toggleDisplay(true);
+*/
+
+document.getElementById('btn1').onclick = function () {
+    document.getElementById('three').style.display = 'none';
+    document.getElementById('three-half').style.display = 'flex';
+
+}
+
+document.getElementById('btn2').onclick = function () {
+    document.getElementById('three-half').style.display = 'none';
+    document.getElementById('three').style.display = 'flex';
+
+}
+
+document.getElementById('threes').onclick = () => {
 
     let fourth = document.getElementById('four');
     fourth.getElementsByTagName('div')[0].setAttribute('class', 'parallax');
@@ -52,6 +92,7 @@ document.getElementById('three').onclick = () => {
     fourth.scrollIntoView({behavior: "smooth"});
 
 }
+
 
 document.getElementById('four').onclick = function () { 
     document.getElementById('one').scrollIntoView({behavior: "smooth"});
