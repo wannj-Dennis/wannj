9b4a7364ac2bba160dfd91574afbe9ccccc088db
Niju <wanjofu26@live.com>
2021-02-09 14:24:02 +0000

    desktop update

--- main.js
+++ main.js
@@ -2,18 +2,7 @@
 // set timeout
 document.getElementById('head-one').innerHTML = 'Wanj Dennis';
 
-// count 
-let marker = 0; 
- const count = (projects) => {
-     setInterval (() => {
-         if (marker != projects){
-             marker += 1;
-             document.getElementById('projects').innerHTML = "<i class='fas fa-medal'></i> Projects " + '<span class="righter">' + marker + '</span>';
-         }
-     }, 500);
- }
 
- count(5);
 
 setTimeout(
     function () {        
@@ -46,6 +35,7 @@ document.getElementById('one').onclick = function clicker ()  {
 document.getElementById('two').onclick = () => {
 
     let thrice = document.getElementById('three');
+    document.getElementById('profile-img').style.width = '30vw';
 
     document.getElementById('two').getElementsByTagName('div')[0].removeAttribute('id', 'slider2');
 
@@ -53,62 +43,102 @@ document.getElementById('two').onclick = () => {
     thrice.getElementsByTagName('div')[0].setAttribute('id', 'slider2');
 }
 
-/*
-const toggleDisplay = (id) => {
-    if (document.getElementById('btn1').clicked == id){
-        document.getElementById('three').style.display = 'none';
-        document.getElementById('three-half').style.display = 'flex';
-    }
-    else if (document.getElementById('btn2').clicked == id)  {
-        document.getElementById('three-half').style.display = 'none';
-        document.getElementById('three').style.display = 'flex';      
-    }
-}
 
-toggleDisplay(true);
-*/
+let third_page = document.querySelector('#profile-img');
+let righty = document.getElementById('right-half');
 
-document.getElementById('btn1').onclick = function () {
-    document.getElementById('three').style.display = 'none';
+
+righty.addEventListener('scroll', function (){
+    //console.log(righty.scrollHeight);
+
+    let vals = righty.scrollTop;   
+
+    if (vals >= 1000 && vals <= 2500){
+        
+        document.getElementById('profile-img').style.width = '30vw';
+        document.getElementById('profile-img').style.transition = 'width, .5s ease-out';
+    }
+
+    else {
+        document.getElementById('profile-img').style.width = '50vw'; 
+        document.getElementById('profile-img').style.transition = 'width, .5s ease-in';
+    }
+
+    //console.log(vals);
+
+        
+});
+
+document.getElementById('btn1').onclick = function (e) {
+    e.stopPropagation();
     document.getElementById('three-half').style.display = 'flex';
-
+    document.getElementById('three').style.display = 'none';
+    //document.getElementsByClassName('rights')[2].style.display = 'none';
+    //document.getElementsByClassName('rights')[3].style.display = 'flex';
 }
 
-document.getElementById('btn2').onclick = function () {
+document.getElementById('btn2').onclick = function (e) {
+    e.stopPropagation();
     document.getElementById('three-half').style.display = 'none';
     document.getElementById('three').style.display = 'flex';
 
 }
 
-document.getElementById('threes').onclick = () => {
 
-    let fourth = document.getElementById('four');
-//    fourth.getElementsByTagName('div')[0].setAttribute('class', 'parallax');
+document.getElementById('three').onclick = () => {
 
-
+    var fourth = document.getElementById('four');
 
     document.getElementById('three').getElementsByTagName('div')[0].removeAttribute('id', 'slider2');
+    fourth.scrollIntoView({ behavior: "smooth" });   
+    //    fourth.getElementsByTagName('div')[0].setAttribute('class', 'parallax');
+}
 
-    fourth.scrollIntoView({behavior: "smooth"});
+
+document.getElementById('backs').onclick = function (e) {
+    e.stopPropagation();
+
+    let x = document.getElementById('first-project');
+    x.classList.add('move-left');
 
 }
 
 
-document.getElementById('forwards').onclick = function () {
-    document.getElementById('first-project').classList.add('move-left');
-    
+document.getElementById('backs').onmouseup = function () {
+    let x = document.getElementById('first-project');
+
+    x.classList.remove('move-right');
+    console.log(x.classList);
 }
 
 
-document.getElementById('backs').onclick = function () {
-    document.getElementById('first-project').classList.add('move-right');
-    
+
+
+document.getElementById('forwards').onclick = function (e) {
+    e.stopPropagation();
+
+    let x = document.getElementById('first-project');
+
+    x.classList.add('move-right');
 }
 
-/*
+
+
+
+document.getElementById('forwards').onmouseup = function () {
+    let x = document.getElementById('first-project');
+
+    x.classList.remove('move-right');
+    console.log(x.classList);
+}
+
+
+
+
 document.getElementById('four').onclick = function () { 
     document.getElementById('one').scrollIntoView({behavior: "smooth"});
+    document.getElementById('profile-img').style.width = '50vw';
 
 }
-*/
+
 
